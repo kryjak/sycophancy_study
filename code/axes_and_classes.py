98d505f22a61f8087e45482d62b87e636d29cb5a
Jakub Krys <jakkrys@gmail.com>
2024-09-03 20:01:04 +0100

    Axes and classes

---
+++ code/axes_and_classes.py
@@ -0,0 +1,13 @@
+axes = ['age', 'gender', 'location']
+
+classes = {
+  'age': ['young', 'old'],
+  'gender': ['male', 'female'],
+  'location': ['western', 'eastern']
+}
+
+affirmative_classes = {
+  'age': 'young',
+  'gender': 'female',
+  'locatioon': 'eastern'
+}
