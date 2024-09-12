75a4d77084fa7d56f38e651e8ea8ae484e5c2392
Jakub Krys <jakkrys@gmail.com>
2024-09-12 21:32:40 +0200

    Adapt prepare_data.py to be compatible with pipeline.py

--- code/prepare_data.py
+++ code/prepare_data.py
@@ -2,18 +2,7 @@ import pandas as pd
 from axes_and_classes import *
 import generate_data as gd
 
-df_train = pd.read_csv('data_source_nlp/input_label_pairs_filtered_train.csv')
-df_test = pd.read_csv('data_source_nlp/input_label_pairs_filtered_test.csv')
-
-if 'input' not in df_train.columns or 'label' not in df_train.columns:
-    raise ValueError("Train DataFrame must contain 'input' and 'label' columns")
-if 'input' not in df_test.columns or 'label' not in df_test.columns:
-    raise ValueError("Test DataFrame must contain 'input' and 'label' columns")
-
-print(df_train.head())
-print(df_test.head())
-
-def create_prompts(axes: list[str]) -> None:
+def create_prompts(df_train: pd.DataFrame, df_test: pd.DataFrame, axes: list[str]) -> None:
     # Convert to dictionaries because `gd.generate_nlp_data_easy` expects a dictionary:
     dict_train = dict(zip(df_train['input'], df_train['label']))
     dict_test = dict(zip(df_test['input'], df_test['label']))
@@ -22,6 +11,7 @@ def create_prompts(axes: list[str]) -> None:
     NUM_EXAMPLES_TEST = len(dict_test)
 
     for axis in axes:
+        print(f'Generating prompts for {axis}...')
         train_prompts_df = gd.generate_nlp_data_easy(dict_train, axis, NUM_EXAMPLES_TRAIN)
         test_prompts_easy_df = gd.generate_nlp_data_easy(dict_test, axis, NUM_EXAMPLES_TEST)
         test_prompts_hard_df = gd.generate_nlp_data_hard(dict_test, axis, NUM_EXAMPLES_TEST)
@@ -31,4 +21,15 @@ def create_prompts(axes: list[str]) -> None:
         test_prompts_hard_df.to_csv(f'data_source_nlp/test_prompts_hard_{axis}.csv', index=False)
 
 if __name__ == '__main__':
-    create_prompts(axes)
+    df_train = pd.read_csv('data_source_nlp/input_label_pairs_filtered_train.csv')
+    df_test = pd.read_csv('data_source_nlp/input_label_pairs_filtered_test.csv')
+
+    if 'input' not in df_train.columns or 'label' not in df_train.columns:
+        raise ValueError("Train DataFrame must contain 'input' and 'label' columns")
+    if 'input' not in df_test.columns or 'label' not in df_test.columns:
+        raise ValueError("Test DataFrame must contain 'input' and 'label' columns")
+
+    print(df_train.head())
+    print(df_test.head())
+
+    create_prompts(df_train, df_test, axes)
